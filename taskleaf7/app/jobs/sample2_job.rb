1fca80ac4f9513a74c26c33b770d335d39e69d15
Oolongpuer <52593491+Oolongpuer@users.noreply.github.com>
2019-11-26 22:33:32 +0900

    first commit

--- taskleaf7/app/jobs/sample2_job.rb
+++
@@ -1,7 +0,0 @@
-class Sample2Job < ApplicationJob
-  queue_as :default
-
-  def perform(*args)
-    Sidekiq::Logging.logger.info "サンプルジョブ2を実行しました"
-  end
-end
--- taskleaf70/db/migrate/20180729123451_add_deleted_at_to_tasks.rb
+++
@@ -1,6 +0,0 @@
-class AddDeletedAtToTasks < ActiveRecord::Migration[5.2]
-  def change
-    add_column :tasks, :deleted_at, :datetime
-    add_index :tasks, :deleted_at
-  end
-end
